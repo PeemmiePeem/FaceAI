25a29036944a3c068f547a9f59f7e76e6ecb827c
Peem <peemxmanuelz@gmail.com>
2025-05-02 12:31:04 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import cv2
+import numpy as np
+from PIL import Image
+st.title("📷 ตรวจจับใบหน้าในภาพ")
+uploaded_file = st.file_uploader("อัปโหลดรูปภาพจากมือถือ", type=["jpg","png"])
+
+if uploaded_file is not None:
+    #อ่านภาพเป็น PIL แล้วแปลเป็น OpenCV
+    image = Image.open(uploaded_file)
+    img_np = np.array(image.convert('RGB'))
+    img = cv2.cvtColor(img_np, cv2.COLOR_RGB2BGR)
+
+    #โหลดตรวจจับใบหน้า
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+
+    #แปลงภาพเป็น greyscale เพื่อนตรวจจับใบหน้า
+    gray=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(
+    gray,
+    scaleFactor=1.1,
+    minNeighbors=9,
+    minSize=(200,200))
+
+    #วาดกรอบรอบใบหน้า
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img_np, (x,y), (x + w,y + h), (255, 0, 0),5)
+
+    st.image(img_np, caption=f"ตรวจพบใบหน้า {len(faces)} คน", use_container_width=True)
+    st.success(f"✅ ตรวจพบใบหน้าทั้งหมด : {len(faces)} คน")
